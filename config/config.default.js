7b59b554060eefec78553aece8bbdfecef8b315e
koto <2650006147@qq.com>
2023-03-29 11:52:42 +0800

    server

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,13 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7001,
+      hostname: '0.0.0.0',
+    }
+  };
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
