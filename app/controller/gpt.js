3590acd10b0827cec91c50c050d1859169b78259
koto <2650006147@qq.com>
2023-04-16 16:54:44 +0800

    aaaa

--- app/controller/gpt.js
+++ app/controller/gpt.js
@@ -4,7 +4,7 @@ class CharGPTController extends Controller {
   async sendMsg() {
     const { ctx } = this;
     const { data } = ctx.request.body
-    const AZURE_GPT_SERVER_ADDRESS = 'http://74.235.203.174:3002/api/v1/sendMsg'
+    const AZURE_GPT_SERVER_ADDRESS = 'http://35.174.111.64:3002/api/v1/sendMsg'
     let msg = '如果我本月支出'
     for (let i = 0; i < data.length; i++) {
       msg = msg + "在" + data[i].typeName + "方面花" + data[i].typeTotal + "元,"
@@ -20,7 +20,7 @@ class CharGPTController extends Controller {
       timeout: 20000,
     })
     console.log(result.data)
-    const bufferOriginal = Buffer.from(JSON.parse(result.data).data)
+    const bufferOriginal = Buffer.from(result.data)
     const bufferString = bufferOriginal.toString('utf8')
     ctx.success(bufferString)
   }
